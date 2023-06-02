e51d6ab5369597833e9f41eaba94a3336678b553
wasborn14 <archibald.ff14@gmail.com>
2023-06-03 07:27:27 +0900

    fix tablayout

--- src/components/organisms/TabList/index.tsx
+++ src/components/organisms/TabList/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Sortable } from './Sortable'
+import styled from 'styled-components'
 
 export const TabList = () => {
   return (
-    <>
+    <Container>
       <Sortable />
-    </>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  margin-left: 10px;
+`
